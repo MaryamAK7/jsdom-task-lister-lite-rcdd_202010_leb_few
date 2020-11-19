32cebdb3a9207b2fe3763350dd92d4206f871cdf
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-11-19 20:17:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ document.addEventListener("DOMContentLoaded", () => {
 document.getElementById("mySubmit").addEventListener("click", function(){
   
   let value= document.getElementById("new-task-description").value;
-  if(value!="")
-  {document.getElementById("tasks").innerHTML+=`<li> ${value}</li>`;}
+  if(value!=="")
+  {document.getElementById("tasks").innerHTML+=`<li class="listElement"> ${value} <button type="button"> Delete </button></li>`;}
 })
+  let buttons = document.getElementsByTagName("button");
+  let list= document.getElementsByClassName("listElement");
+for(let i=0;i<buttons.length;i++){
+document.buttons[i].addEventListener("click",but);
+}
+function but(){
+  for(i=0;i<list.length;i++){
+  document.list[i].innerText=" ";
+}
+}
+  
+  
 });
